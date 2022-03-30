0ceffa57c770a0780d46899dbd2bafec8e6a9f10
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-30 10:12:10 +0300

    Initial Commit

---
+++ HeaderAndFooter/Collection/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HeaderAndFooter
+//
+//  Created by Никита Коголенок on 30.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
